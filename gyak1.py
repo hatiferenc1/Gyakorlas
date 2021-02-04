8fbdd90e46486a3d244f59bb284af30c4f43f3fa
hatiferenc1 <hati.ferenc@wm-iskola.hu>
2021-02-04 12:36:23 +0000

    Initial commit

---
+++ gyak1.py
@@ -0,0 +1,12 @@
+'''Videóban látott feladat elkészítése (sulipy 3. fejezet. Változók és típusok. Első lépések)
+
+a videóban 6. percnél tartok
+A videóban látott videó kis elemzése.
+
+a változó neve
+tartalmazhat betűket,számokat,aláhúzásokat,viszont nem lehet szám az elején
+
+Létrehoztunk egy változót(egy dobozt)aminek adtunk egy feliratot 'szöveg' felirat 
+a print-el ki tudjuk őket iratni.'''' 
+
+print('Ez egy hosszú szöveg')
---
+++ main.py
@@ -0,0 +1,4 @@
+_ez_egy_hosszabb_szöveg9 ='Ez egy hosszú szöveg'
+szám = 27
+print(_ez_egy_hosszabb_szöveg9)
+print(szám)
